1643c3f453e526adb32936ed641a3fab463cfe73
vitorbite <vitorbitencourt20@gmail.com>
2026-06-23 20:56:51 -0300

    Criando o método get e post

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const app = express();
-const PORT = 3001
+const PORT = 3001;
+app.use(express.json());
+const produtos = [];
+let idCounter = 1;
 
-app.get("/", (req, res)=>{
-    res.send("Olá, Mundo");
+function validarNome(nome) {
+  return typeof nome === "string" && nome.trim().length > 0;
+}
+
+app.post("/produtos", (req, res) => {
+  try {
+    const { nome } = req.body;
+
+    if (!validarNome(nome)) {
+      return res
+        .status(400)
+        .json({
+          erro: "O campo 'nome' é obrigatório e deve ser uma string válida.",
+        });
+    }
+    const novoProduto = { id: idCounter, nome };
+    produtos.push(novoProduto);
+
+    res.status(201).json(novoItem);
+  } catch (error) {
+    res.status(500).json({ erro: "Erro interno do servidor." });
+  }
 });
+app.get("/produtos", (req, res) => {
+  res.json(produtos);
+});
+app.put("/produtos", (req, res) => {});
+app.delete("/produtos", (req, res) => {});
 
-app.listen(PORT, ()=>{
-    console.log("Servidor iniciado na porta: " + PORT);
+app.listen(PORT, () => {
+  console.log("Servidor iniciado na porta: " + PORT);
 });
